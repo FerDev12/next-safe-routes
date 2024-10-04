4e8c134ac8ef2808ae94a67f4a69daf8261df8bc
Fernando Muñoz <fernando.mu.dev@protonmail.com>
2024-10-04 14:12:37 -0600

    update plugin to ensure it only runs once per command execution

--- packages/next-safe-routes/src/lib/plugin/index.ts
+++ packages/next-safe-routes/src/lib/plugin/index.ts
@@ -1,21 +1,23 @@
-// src/next-plugin.ts
-
 import { NextConfig } from 'next';
 import { generateRoutes } from '@/core';
 import path from 'path';
+import webpack from 'webpack';
 
-interface TypedRoutesConfig {
+interface SafeRoutesConfig {
   useSrcDirectory?: boolean;
   outputPath?: string;
 }
 
+// This flag will be reset at the start of each build
+let hasGeneratedRoutes = false;
+
 export function withNextSafeRoutes(
   nextConfig: NextConfig = {},
-  typedRoutesConfig?: TypedRoutesConfig
+  safeRoutesConfig?: SafeRoutesConfig
 ): NextConfig {
-  const config: TypedRoutesConfig = {
-    useSrcDirectory: typedRoutesConfig?.useSrcDirectory ?? true,
-    outputPath: typedRoutesConfig?.outputPath ?? 'types/routes.ts',
+  const config: SafeRoutesConfig = {
+    useSrcDirectory: safeRoutesConfig?.useSrcDirectory ?? true,
+    outputPath: safeRoutesConfig?.outputPath ?? 'types/routes.ts',
   };
 
   const { useSrcDirectory, outputPath } = config;
@@ -24,22 +26,36 @@ export function withNextSafeRoutes(
     ...nextConfig,
     webpack: (config, options) => {
       if (options.isServer) {
-        const rootDir = process.cwd();
-        const srcDir = useSrcDirectory ? 'src' : '';
-        const fullPagesDir = path.join(rootDir, srcDir, 'app');
-        const fullOutputPath = path.join(
-          rootDir,
-          srcDir,
-          outputPath
-            ? outputPath.startsWith('src') ||
-              outputPath.startsWith('./src') ||
-              outputPath.startsWith('/src')
-              ? outputPath.slice(outputPath.indexOf('c') + 1)
-              : outputPath
-            : 'types/routes.ts'
+        // Reset the flag at the start of each build
+        config.plugins.push(
+          new webpack.ProgressPlugin((percentage) => {
+            if (percentage === 0) {
+              hasGeneratedRoutes = false;
+            }
+          })
         );
 
-        generateRoutes(fullPagesDir, fullOutputPath);
+        // Generate routes only if they haven't been generated in this build
+        if (!hasGeneratedRoutes) {
+          const rootDir = process.cwd();
+          const srcDir = useSrcDirectory ? 'src' : '';
+          const fullPagesDir = path.join(rootDir, srcDir, 'app');
+          const fullOutputPath = path.join(
+            rootDir,
+            srcDir,
+            outputPath
+              ? outputPath.startsWith('src') ||
+                outputPath.startsWith('./src') ||
+                outputPath.startsWith('/src')
+                ? outputPath.slice(outputPath.indexOf('c') + 1)
+                : outputPath
+              : 'types/routes.ts'
+          );
+
+          generateRoutes(fullPagesDir, fullOutputPath);
+          hasGeneratedRoutes = true;
+          console.log('Routes generated successfully for this build.');
+        }
       }
 
       if (typeof nextConfig.webpack === 'function') {
